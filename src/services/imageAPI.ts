fc028ac2bf51c1cbb8a3855c7a35edf4f91f0212
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-08-28 20:41:28 +0530

    feat: add mock image fetching function if API failed

--- src/services/imageAPI.ts
+++ src/services/imageAPI.ts
@@ -1,19 +1,38 @@
 const generateImage = async (prompt: string) => {
-  const API_URL =
-    'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0';
-  const response = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${import.meta.env.REACT_APP_HF_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ inputs: prompt }),
-  });
+    try {
+        const API_URL =
+            'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0';
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${import.meta.env.REACT_APP_HF_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ inputs: prompt }),
+        });
 
-  const blob = await response.blob();
-  const url = URL.createObjectURL(blob);
-  return url;
+        if (!response.ok) {
+            throw new Error('Something went wrong');
+        }
+
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        return url;
+    } catch (error) {
+        return getMockImage();
+    }
 };
 
+const getMockImage = async () => {
+    try {
+        const image = await fetch('https://unsplash.it/640/425?random');
+        if (!image.ok) {
+            throw new Error('Error while fetching mock image');
+        }
+        return image.url;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export { generateImage };
